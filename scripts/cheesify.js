435b15c7a0f343b160331cdb305bc9acabb67975
Lucile Bouchire <l.bouchire@gmail.com>
2023-11-29 13:45:38 +0100

    Declaring cheesify function

--- scripts/cheesify.js
+++ scripts/cheesify.js
@@ -1,6 +1,13 @@
+// cheesify function making image replacement
+
 function cheesify() {
   // TODO: Add the image replacement script here
+  document.querySelectorAll("img").forEach((img) => {
+    img.src = `https://source.unsplash.com/collection/8884129/${img.width}x${
+      img.height
+    }?${Math.random()}`;
+    img.srcset = img.src;
+  });
 }
 
 // TODO: Write a function to listen for messages on the content page using chrome.runtime.onMessage
-  
